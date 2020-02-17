dd6d8c2f170f5c87c27cf328e401b84dfa668641
zhangyang <18203699419@163.com>
2020-02-17 21:22:38 +0800

    third commit!

--- iess/Program.cs
+++ iess/Program.cs
@@ -6,7 +6,9 @@ namespace iess
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("你好世界啊!");
+            string aaa = "打篮球";
+            string bbb = "游泳";
+            Console.WriteLine("我喜欢{0},但是更爱{1}!",aaa,bbb);
         }
     }
 }
